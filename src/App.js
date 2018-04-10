c7b97f90263e6634db4d64684332fffb29b9a3da
Andrei Motinga <andrew.motinga@gmail.com>
2018-04-09 20:55:24 -0400

    add urls

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: "",
+      version: ""
+    }
+  }
+
+  componentDidMount() {
+    axios.get(process.env.REACT_APP_API_URL)
+      .then(res => {
+        this.setState({
+          name: res.data.name,
+          version: res.data.version
+        });
+      })
+  }
+
   render() {
+    const url = process.env.REACT_APP_API_URL
     return (
       <div className="App">
         <header className="App-header">
@@ -11,6 +31,11 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
+          <ul>
+            <li>name is {this.state.name}</li>
+            <li>v is {this.state.version}</li>
+          </ul>
+          br
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
